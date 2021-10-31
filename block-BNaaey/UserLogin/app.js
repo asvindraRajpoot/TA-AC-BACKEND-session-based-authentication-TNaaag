5155f6aad1631781bf6ea86213a9e8fa753da5cc
asvindra <aswin1399904@gmail.com>
2021-10-31 18:18:59 +0530

    updated the session

--- block-BNaaey/UserLogin/app.js
+++ block-BNaaey/UserLogin/app.js
@@ -5,21 +5,24 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose=require('mongoose');
 var session=require('express-session');
-var MongoStore=require('connect-mongo')(session);
+var MongoStore = require('connect-mongo')(session);
 
-require('dotenv').config()
+//require('dotenv').config()
 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
 
-mongoose.connect('mongodb://localhost/users',(err)=>{
+
+mongoose.connect('mongodb://localhost/users',{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+},(err)=>{
   console.log(err?err:'Connected to Database');
 })
 
-
+var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -34,12 +37,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //add session and persit user in session
 app.use(session({
-   secret:process.env.SECRET,
+   secret:'somerandomsecret',
    resave:false,
    saveUninitialized:false,
-   store: new  MongoStore({mongooseConnection:mongoose.connection})
-
+   store: new MongoStore({mongooseConnection:mongoose.connection})
 }))
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
